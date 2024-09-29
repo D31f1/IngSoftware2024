208bd2d669daba4c3cc7bbc44d73caff43183d72
NazarenaCalvo <nazacalvo15@gmail.com>
2024-09-29 15:38:56 -0300

    Correccion de registro
    Ahora permite guardar un usuario con todos sus atributos en la BD

--- MegastoreFront/src/Componentes/Registro/Registro.js
+++ MegastoreFront/src/Componentes/Registro/Registro.js
@@ -9,13 +9,14 @@ const Registro = () => {
     nombre: '',
     apellido: '',
     nroTelefono: '',  // Número de teléfono del usuario
-    direcciones: [{ calle: '', numero: '' }],  // Lista de direcciones con calle y número
+    direcciones: [],  // Lista de direcciones con calle y número
     correo: '',  // Correo electrónico del usuario
     contrasenia: '',  // Contraseña ingresada
     confirmarContrasenia: ''  // Confirmación de la contraseña
   });
-  const [newAddress, setNewAddress] = useState({ calle: '', altura: '' });
+  const [newAddress, setNewAddress] = useState({ calle: '', numero: '' });
   const [showDirections, setShowDirections] = useState(false);
+  const [addressError] = useState(''); // Estado para manejar errores de dirección
   const navigate = useNavigate();// Hook de React Router para redireccionar
 
   // Manejador para los cambios en los campos del formulario
@@ -24,20 +25,30 @@ const Registro = () => {
     setFormData({ ...formData, [name]: value });
   };
 
+  
   // Manejador para cambios en las direcciones de envío (calle y número)
-  const handleNewAddressChange = (index, field, value) => {
+  const handleNewAddressChange = (field, value) => {
     setNewAddress({ ...newAddress, [field]: value });
   };
 
   // Añade una nueva dirección de envío
   const handleAddAddress = () => {
-    if (newAddress.calle && newAddress.altura) {
-      setFormData({
-        ...formData,
-        direcciones: [...formData.direcciones, newAddress]
-      });
-      setNewAddress({ calle: '', altura: '' });
+    // Verifica si ambos campos (calle y numero) están vacíos
+    if (!newAddress.calle && !newAddress.numero) {
+      alert('Debes ingresar una calle o numero.');; // Establece el mensaje de error para ambos campos vacios
+      return;
     }
+    // Verificar si solo un campo está vacío
+    if (!newAddress.calle || !newAddress.numero){
+      alert('Debes completar ambos campos: calle y numero.');// Mostrar error si uno está vacío
+    }
+    //setAddressError(''); //Limpiar el error si ambos campos estan completos
+    setFormData({
+      ...formData,
+      direcciones: [...formData.direcciones, newAddress]
+    });
+    setNewAddress({ calle: '', numero: '' });
+    
   };
 
   // Elimina una dirección de envío por índice
@@ -54,7 +65,15 @@ const Registro = () => {
   // Manejador para el envío del formulario
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+
+    // Verifica si la lista de direcciones está vacía
+    if (formData.direcciones.length === 0) {
+      alert('Debes añadir al menos una dirección de envío.');
+      return; // Evita el envío si no hay direcciones
+    }
+    
+    registrarUsuario(); // Llama a la función para registrar el usuario
+    
   };
 
   const registrarUsuario = () => {
@@ -84,7 +103,7 @@ const Registro = () => {
       .then((data) => {
         console.log('Usuario registrado:', data);
         alert('Registro exitoso');
-        navigate('/Home');  // Redirecciona a la página de inicio después del registro exitoso
+        navigate('/');  // Redirecciona a la página de inicio después del registro exitoso
       })
       .catch((error) => {
         console.error('Error:', error);
@@ -96,7 +115,7 @@ const Registro = () => {
   return (
     <div className='register-container'>
       <div><Cabecera /></div>
-      <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" className="registro-svg" viewBox="0 0 16 16">
+      <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fillRule="currentColor" className="registro-svg" viewBox="0 0 16 16">
         <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
       </svg>
       <h1>REGÍSTRATE</h1>
@@ -143,31 +162,29 @@ const Registro = () => {
               className="calle-input"
               value={newAddress.calle}
               onChange={(e) => handleNewAddressChange('calle', e.target.value)}
-              required
               placeholder="Calle"
             />
             <input
               type="text"
               className="altura-input"
-              value={newAddress.altura}
-              onChange={(e) => handleNewAddressChange('altura', e.target.value)}
-              required
+              value={newAddress.numero}
+              onChange={(e) => handleNewAddressChange('numero', e.target.value)}
               placeholder="Altura"
             />
             <button type="button" className="add-button" onClick={handleAddAddress}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fillRule="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
               </svg>
             </button>
             <button type="button" className='mostrar-ocultar-button' onClick={() => setShowDirections(!showDirections)}>
               {showDirections ? (
                 // SVG para ocultar (flecha hacia arriba)
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-up" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="bi bi-chevron-up" viewBox="0 0 16 16">
                   <path fillRule="evenodd" d="M1.646 11.354a.5.5 0 0 0 .708 0L8 5.707l5.646 5.647a.5.5 0 0 0 .708-.708l-6-6a.5.5 0 0 0-.708 0l-6 6a.5.5 0 0 0 0 .708z" />
                 </svg>
               ) : (
                 // SVG para mostrar (flecha hacia abajo)
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="bi bi-chevron-down" viewBox="0 0 16 16">
                   <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
                 </svg>
               )}
@@ -181,9 +198,9 @@ const Registro = () => {
               {formData.direcciones.map((direccion, index) => (
                 <div key={index} className="address-item">
                   <span>{direccion.calle}</span>
-                  <span>{direccion.altura}</span>
+                  <span>{direccion.numero}</span>
                   <button type="button" className="cancel-button" onClick={() => handleRemoveAddress(index)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fillRule="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
                       <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
                     </svg>
                   </button>
@@ -191,6 +208,9 @@ const Registro = () => {
               ))}
             </div>
           )}
+          <div>
+          {addressError && <div className="error-message">{addressError}</div>} {/* Mensaje de error de dirección */}
+          </div>
         </div>
 
         <div className="register-group-email">
